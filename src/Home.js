318788b55d8673cc4cd8b40c4ecc1e13431f8f14
Ali Luhar <ali.luhar.2001@gmail.com>
2025-01-27 15:58:28 +0530

    port change for client

--- src/Home.js
+++ src/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
   useEffect(() => {
     const fetchWallpapers = async () => {
       try {
-        const response = await axios.get('http://localhost:5000/wallpapers');
+        const response = await axios.get('http://localhost:10000/wallpapers');
         setWallpapers(response.data);
         setLoading(false);
       } catch (error) {
